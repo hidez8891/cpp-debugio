80c9183aea0cbbfb6663bbbaae3867c13439ed1f
hidez8891 <427357+hidez8891@users.noreply.github.com>
2021-09-21 19:54:40 +0900

    fixed process/thread priority

--- example/dcat.cpp
+++ example/dcat.cpp
@@ -25,6 +25,16 @@ int main()
         return 0;
     });
 
+#ifdef _MSC_VER
+    SetPriorityClass(::GetCurrentProcess(), REALTIME_PRIORITY_CLASS);
+    SetThreadPriority(monitor.native_handle(), THREAD_PRIORITY_TIME_CRITICAL);
+#else
+    struct sched_param param = {
+        .sched_priority = 99,
+    };
+    pthread_setschedparam(monitor.native_handle(), SCHED_FIFO, &param);
+#endif
+
     std::unique_lock<std::mutex> lk(mtx);
     do_shutdown.wait(lk); // wait for shutdown
 
